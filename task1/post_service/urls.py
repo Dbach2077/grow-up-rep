b3a75b65b0ac758da28bf99610268a15e912f248
Dbach2077 <Dbach2077@gmail.com>
2025-08-18 14:33:50 +0300

    feat(ui): Implement ExtJS interface for record management

--- task1/post_service/urls.py
+++ task1/post_service/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from parcels.views import IndexView 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('parcels.urls'))
+    path('api/v1/', include('parcels.urls')),
+    path('', IndexView.as_view(), name='index'), 
 ]
